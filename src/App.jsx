4d6f46385f0327a3685ab43121234fec97ecd49c
Benedict Tedjokusumo <bentaro89@gmail.com>
2021-05-26 18:46:53 -0400

    basketball worked

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import React, { useState } from 'react';
 
-import logo from './logo.svg';
 import NavBar from './components/NavBar'
 import Basketball from './assets/basketball.svg'
 import BasketballHoop from './assets/basketballHoop.svg'
+import rimPart from './assets/betterHoop.svg'
 import './App.css';
 
 function App() {
@@ -25,11 +25,12 @@ function App() {
     <div className='App'>
       <div className='App-header'>
         <NavBar/>
-        {/* <img src={logo} className='App-logo' alt='logo' /> */}
         <div className='basketball-container'>
           <img src={Basketball} className={ball ? 'basketball-ball active' : 'basketball-ball'} alt='basketball'/>
-          <img src={BasketballHoop} className='basketball-hoop' alt='hoop'/>
         </div>
+        <img src={BasketballHoop} className='basketball-hoop' alt='hoop'/>
+        <img src={rimPart} className='basketball-rim' alt='hoop'/>
+
         
       </div>
     </div>

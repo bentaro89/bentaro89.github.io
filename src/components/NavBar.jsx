89e20bd3a5bc53b4cfa0eeb997012d624e1db2b9
Ben <bentaro89@gmail.com>
2021-06-12 23:38:26 -0700

    fixed resume

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -10,7 +10,7 @@ const NavBar = () => {
             <div className='nav-items'>
             <a className='nav-item' href="#about">About</a>
             <a className='nav-item' href="#projects">Projects</a>
-            <a className='nav-item' target='blank' href={Resume} download={Resume}>Download Resume</a>
+            <a className='nav-item' target='_blank' href={Resume}>Resume</a>
             </div>
             
         </div>
